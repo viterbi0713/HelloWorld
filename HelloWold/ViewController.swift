efcd3f09885c3b225dc5c1c7bb64551bef6b518a
Toru Yoshihara <yoshihara@viterbimbp.local>
2016-09-01 10:45:26 +0900

    Initial Commit

---
+++ HelloWold/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWold
+//
+//  Created by Toru Yoshihara on 2016/09/01.
+//  Copyright © 2016年 tooru.yoshihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
